ee58afaf73dfa77aca00d9a8369fc5443ea10108
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-06-18 21:08:13 -0700

    implemented bcrypt

--- python_stack/django/django_full_stack/login_reg/main/views.py
+++ python_stack/django/django_full_stack/login_reg/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import User
-
+import bcrypt
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -12,11 +12,14 @@ def register(request):
         for error in errors.values():
             messages.error(request, error)
         return redirect('/')
+    hashed = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
+    print(request.POST['password'])
+    print(hashed)
     this_user = User.objects.create( 
         first_name=request.POST['first_name'],
         last_name=request.POST['last_name'],
         email=request.POST['email'],
-        password=request.POST['password'],
+        password=hashed
     )
     request.session['user_id'] = this_user.id
     return redirect('/success')
