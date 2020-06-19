a25b0d0363f443b29e91b47649b0b59ae0da6ca8
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-06-19 00:20:47 -0700

    set up necessary models

--- python_stack/django/django_full_stack/the_wall/main/models.py
+++ python_stack/django/django_full_stack/the_wall/main/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+import re
 
 # Create your models here.
+
+class UserManager(models.Manager):
+    def register_validator(self, post_data):
+        errors = {}
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if len(post_data['first_name']) < 2:
+            errors['first_name'] = "First name MUST be at least 3 characters long!"
+        if len(post_data['last_name']) < 4:
+            errors['last_name'] = "Last name MUST be at least 5 characters long!"
+        if not EMAIL_REGEX.match(post_data['email']):
+            errors['email'] = "That doesn't look like an email!"
+        if len(post_data['password']) < 2:
+            errors['password'] = "Pasword MUST be at least 3 characters long!"
+        return errors
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
+
+class Message(models.Model):
+    content = models.CharField(max_length=255)
+    poster = models.ForeignKey(User, related_name="messages", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    content = models.CharField(max_length=255)
+    commented_on = models.ForeignKey(Message, related_name="comments", on_delete=models.CASCADE)
+    writer = models.ForeignKey(User, related_name="comments", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+
--- python_stack/django/django_full_stack/the_wall/main/views.py
+++ python_stack/django/django_full_stack/the_wall/main/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import User
+import bcrypt
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    errors = User.objects.register_validator(request.POST)
+    if len(errors) > 0:
+        for error in errors.values():
+            messages.error(request, error)
+        return redirect('/')
+    hashed = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
+    this_user = User.objects.create( 
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        email=request.POST['email'],
+        password=hashed
+    )
+    request.session['user_id'] = this_user.id
+    return redirect('/')
+
+def login(request):
+    users = User.objects.filter(email=request.POST['email'])
+    if users:
+        logging_in_user = users[0]
+        if bcrypt.checkpw(request.POST['password'].encode(), logging_in_user.password.encode()):
+            request.session['user_id'] = logging_in_user.id
+            return redirect('/')
+    messages.error(request, "Email not found")
+    return redirect('/')
+
+def logout(request):
+    request.session.flush()
+    return redirect('/')
